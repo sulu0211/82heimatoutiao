3e0460381bb48e5e34c9e428a260c2ff91873e0f
sulu <53858647+sulu0211@users.noreply.github.com>
2019-08-31 10:41:35 +0800

    图片

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+<!-- App.vue只放一个根组件 显示组件内容 -->
     <router-view/>
-  </div>
 </template>
 
 <style lang="less">
