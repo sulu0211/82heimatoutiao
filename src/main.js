c43263890900af9e15536cd2a4db71002db06c96
sulu <53858647+sulu0211@users.noreply.github.com>
2019-08-31 14:26:57 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less' // 引入初始化样式 包括 margin padding li
 
 Vue.config.productionTip = false
+Vue.use(ElementUI) // 全局注册ElementUI组件 任意位置都可以使用
 
 new Vue({
   router,
---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <dir>
+      home
+  </dir>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
