125ca8da7705b5800c7de0e775e8393ccc8fad46
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-01 10:05:42 +0800

    home页面样式初始化 git commit -m home页面样式初始化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <dir>
-      home
-  </dir>
+<!-- 住容器 -->
+<el-container>
+  <!-- 左边边栏 -->
+  <el-aside>左侧</el-aside>
+  <!-- 右边栏 -->
+  <el-container>
+    <el-header>顶部</el-header>
+    <el-main>主体</el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
