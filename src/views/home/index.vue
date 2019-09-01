30a47940a07cb18350e7a19f8104a2e77e595a75
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-01 19:42:48 +0800

    主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,26 @@
 <template>
-<!-- 住容器 -->
-<el-container>
-  <!-- 左边边栏 -->
-  <el-aside>左侧</el-aside>
-  <!-- 右边栏 -->
+  <!-- 主容器 -->
   <el-container>
-    <el-header>顶部</el-header>
-    <el-main>主体</el-main>
+    <!-- 左边边栏 -->
+    <el-aside>
+      <layout-aside></layout-aside>
+    </el-aside>
+    <!-- 右边栏 -->
+    <el-container>
+      <el-header>顶部</el-header>
+      <el-main>主体</el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout.aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
 <style>
-
 </style>
