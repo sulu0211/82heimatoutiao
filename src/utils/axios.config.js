b0bf51cdb5cd77fb7bb7eec381571b4b36add30e
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-02 15:40:09 +0800

    接口访问-axios拦截器-统一处理响应数据

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,5 +1,6 @@
 // 放置axios拦截器 请求拦截器和响应拦截器
 import axios from 'axios'
+// 请求拦截器
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 axios.interceptors.request.use(function (config) {
   // config 是axios默认请求配置 和传入的配置 结合出来
@@ -8,7 +9,19 @@ axios.interceptors.request.use(function (config) {
   config.headers['Authorization'] = `Bearer ${token}` // 统一注入token
   // debugger
   return config
-}, function () {
+}, function () {})
 
+// 响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做处理
+  debugger
+  return response.data ? response.data : {}
+}, function (error) {
+  // 对响应错误做处理
+  return Promise.reject(error)
 })
-export default axios
+export default {
+  install (Vue) {
+    Vue.prototype.$axios = axios
+  }
+}
