cee60ce9c7c596f25e758cd9d7ebc8c929bfc8c0
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-02 10:34:43 +0800

    主页模块-左侧导航配置路由

--- src/components/home/layout-aside.vue
+++ src/components/home/layout-aside.vue
@@ -1,10 +1,17 @@
 <template>
+<!-- 是否使用 vue-router 的模式，
+启用该模式会在激活导航时以 index 作为 path 进行路由跳转 -->
   <div class="layout-aside">
     <div class="title">
       <img src="../../assets/img/logo_admin.png" alt />
     </div>
-    <el-menu background-color="#323745" text-color="#adafb5" active-text-color="#ffd04b">
-      <el-menu-item>
+    <el-menu
+    background-color="#323745"
+    text-color="#adafb5"
+    active-text-color="#ffd04b"
+    router
+    >
+      <el-menu-item index="/">
         <i class="el-icon-s-home"></i>
         <span slot="title">首页</span>
       </el-menu-item>
@@ -14,22 +21,22 @@
           <i class="el-icon-s-order"></i>
           <span>内容管理</span>
         </template>
-        <el-menu-item>发布文章</el-menu-item>
-        <el-menu-item>内容列表</el-menu-item>
-        <el-menu-item>评论列表</el-menu-item>
-        <el-menu-item>素材管理</el-menu-item>
+        <el-menu-item index="/home/publish">发布文章</el-menu-item>
+        <el-menu-item index="/home/articles">内容列表</el-menu-item>
+        <el-menu-item index="/home/comment">评论列表</el-menu-item>
+        <el-menu-item index="/home/material">素材管理</el-menu-item>
       </el-submenu>
       <el-submenu>
         <template slot="title">
           <i class="el-icon-s-custom"></i>
           <span>粉丝管理</span>
         </template>
-        <el-menu-item>图片数据</el-menu-item>
-        <el-menu-item>粉丝列表</el-menu-item>
-        <el-menu-item>粉丝画像</el-menu-item>
-        <el-menu-item>粉丝概括</el-menu-item>
+        <el-menu-item index='/home/fansdata'>图片数据</el-menu-item>
+        <el-menu-item index='/home/fanslist'>粉丝列表</el-menu-item>
+        <el-menu-item index='/home/fansimg'>粉丝画像</el-menu-item>
+        <el-menu-item index='/home/fansinfo'>粉丝概括</el-menu-item>
       </el-submenu>
-      <el-menu-item>
+      <el-menu-item index="/home/account">
         <i class="el-icon-s-tools"></i>
         <span slot="title">账户信息</span>
       </el-menu-item>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,7 +10,9 @@
       <el-header>
         <layout-header></layout-header>
       </el-header>
-      <el-main>主体</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
