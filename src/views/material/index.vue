fbf5aa677f677f438940e973e34fd48c0959cd72
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-04 21:54:15 +0800

    黑马头条PC-素材列表-分页组件

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-card>
+  <el-card class="material">
       <bread-crumb>
         <template slot="title">
             素材管理
@@ -8,6 +8,16 @@
       <el-tabs v-model="activeName">
     <el-tab-pane label="全部素材" name="all">
       <!-- 全部素材内容 -->
+      <!-- {{list.length}} -->
+      <div class="card-list">
+        <el-card v-for="item in list" :key="item.id" class="img-card">
+           <img :src="item.url" alt="">
+           <div type="flex" justify="space-around" class="operate" align="middle">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+           </div>
+        </el-card>
+      </div>
     </el-tab-pane>
     <el-tab-pane label="收藏图片" name="collect">
       <!-- 收藏素材内容 -->
@@ -20,12 +30,61 @@
 export default {
   data () {
     return {
-      activeName: 'all'
+      activeName: 'all',
+      list: []
     }
+  },
+  methods: {
+    getMaterial () {
+      this.$axios({
+        url: '/user/images',
+        params: {
+          collect: false
+        }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    }
+  },
+  created () {
+    this.getMaterial()
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.material {
+  .upload-btn {
+    position: absolute;
+    right:10px;
+    margin-top: -10px;
+  }
+  .card-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    .img-card {
+      width: 180px;
+      height: 180px;
+      margin: 30px;
+      position: relative;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+      .operate {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left:0;
+        height: 30px;
+        background-color: #f4f5f6;
+        font-size: 18px;
+        .el-icon-star-on{
+          margin-right:70px;
+        }
+      }
+    }
+  }
+}
 </style>
