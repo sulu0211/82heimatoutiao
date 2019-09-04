ac3bd3aaae6211d96481a59a95e60d6a35c757ba
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-04 20:43:44 +0800

    黑马头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -5,12 +5,24 @@
             素材管理
         </template>
       </bread-crumb>
+      <el-tabs v-model="activeName">
+    <el-tab-pane label="全部素材" name="all">
+      <!-- 全部素材内容 -->
+    </el-tab-pane>
+    <el-tab-pane label="收藏图片" name="collect">
+      <!-- 收藏素材内容 -->
+    </el-tab-pane>
+  </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
