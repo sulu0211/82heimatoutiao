60102b1941ea234bff1832a0d4aca00ac7cb7752
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-05 10:07:03 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,62 +1,59 @@
 <template>
   <el-card class="material">
-      <bread-crumb>
-        <template slot="title">
-            素材管理
-        </template>
-      </bread-crumb>
-      <el-tabs v-model="activeName" @tab-click="changeTab">
+    <bread-crumb slot="header">
+      <template slot="title">素材管理</template>
+    </bread-crumb>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
       <el-tab-pane label="全部素材" name="all">
-      <!-- 全部素材内容 -->
-      <!-- {{list.length}} -->
-      <div class="card-list">
-        <el-card v-for="item in list" :key="item.id" class="img-card">
-           <img :src="item.url" alt="">
-           <div type="flex" justify="space-around" class="operate" align="middle">
+        <!-- 全部素材内容 -->
+        <!-- {{list.length}} -->
+        <div class="card-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+            <div type="flex" justify="space-around" class="operate" align="middle">
               <i :style="{color:item.is_collected ? 'red' : ''}" class="el-icon-star-on"></i>
               <i class="el-icon-delete-solid"></i>
-           </div>
-        </el-card>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="收藏图片" name="collect">
-      <!-- 收藏素材内容 -->
-      <div class="card-list">
-        <el-card v-for="item in list" :key="item.id" class="img-card">
-           <img :src="item.url" alt="">
-           <div type="flex" justify="space-around" class="operate" align="middle">
-              <i :style="{color:item.is_collected ? 'red' : ''}" class="el-icon-star-on"></i>
-              <i class="el-icon-delete-solid"></i>
-           </div>
-        </el-card>
-      </div>
-    </el-tab-pane>
-  </el-tabs>
-  <el-row type="flex" justify="center" style="margin:10px 0">
-       <!-- 分页组件  current-page当前页码 每页显示多少条 page-size每页显示的条数 total 总数 -->
-       <el-pagination  @current-change="changePage" :current-page="page.page" :page-size="page.pageSize" :total="page.total"  background layout="prev, pager, next">
-
-</el-pagination>
+            </div>
+          </el-card>
+        </div>
+      </el-tab-pane>
+      <el-tab-pane label="收藏图片" name="collect">
+        <!-- 收藏素材内容 -->
+        <div class="card-list">
+          <el-card v-for="item in list" :key="item.id" class="img-card">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+    <!-- 分页 -->
+    <el-row type="flex" justify="center">
+      <el-pagination @current-change="changePage" :current-page="page.page" :page-size="pageSize" :total="page.total" background layout="prev, pager, next" ></el-pagination>
     </el-row>
   </el-card>
 </template>
-
 <script>
 export default {
   data () {
     return {
       activeName: 'all',
       list: [],
-
       page: {
-        page: 1
-
+        page: 1,
+        pageSize: 10,
+        total: 0
       }
     }
   },
   methods: {
+    // 切换分页
+    changePage (newPage) {
+      this.page.page = newPage
+      this.getMaterial()
+    },
     // 切换页签
     changeTab () {
+      this.page.page = 1
       // this.activeName是最新的页签
       // 加载不同类型的数据 all=》所有的数据 collect=》去加载收藏数据
       this.getMaterial()
@@ -65,11 +62,14 @@ export default {
       this.$axios({
         url: '/user/images',
         params: {
+          page: this.page.page, // 默认第一次是第一页
+          per_page: this.page.pageSize, // 默认第一页为10条
           // collect: false
-          collect: this.activeName === 'collect'// collect 为true时是加载收藏页面，为false是全部加载页面
+          collect: this.activeName === 'collect' // collect 为true时是加载收藏页面，为false是全部加载页面
         }
       }).then(result => {
-        this.list = result.data.results
+        this.list = result.data.results// 接收数据
+        this.page.total = result.data.total_count// 将图片总数据赋值给总页
       })
     }
   },
@@ -78,12 +78,11 @@ export default {
   }
 }
 </script>
-
 <style lang="less" scoped>
 .material {
   .upload-btn {
     position: absolute;
-    right:10px;
+    right: 10px;
     margin-top: -10px;
   }
   .card-list {
@@ -103,12 +102,12 @@ export default {
         position: absolute;
         width: 100%;
         bottom: 0;
-        left:0;
+        left: 0;
         height: 30px;
         background-color: #f4f5f6;
         font-size: 18px;
-        .el-icon-star-on{
-          margin-right:70px;
+        .el-icon-star-on {
+          margin-right: 70px;
         }
       }
     }
