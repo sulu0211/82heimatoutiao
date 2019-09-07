04de5fec3777e53ae26ae1b4410d2215ee1c36c7
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-07 18:04:50 +0800

    黑马头条PC-404页面应用

--- src/router.js
+++ src/router.js
@@ -3,11 +3,15 @@ import Router from 'vue-router'
 import Home from './views/home'
 import Login from './views/login'
 import Main from './views/home/main.vue'
+import NotFound from './views/404.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
+    path: '*', // 匹配所有找不到的页面
+    component: NotFound
+  }, {
     path: '/login',
     component: Login
   },
