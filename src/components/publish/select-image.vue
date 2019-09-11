44fd2e5c43cf19df7ef30bfb1bdd9651b756c6a1
sulu <53858647+sulu0211@users.noreply.github.com>
2019-09-11 11:17:40 +0800

    发布文章-封面图片选择组件上传

--- src/components/publish/select-image.vue
+++ src/components/publish/select-image.vue
@@ -1,18 +1,28 @@
 <template>
   <el-tabs v-model="activeName">
+    <!-- 全部素材 -->
     <el-tab-pane label="全部素材" name="all">
-       <div class="img-list">
-           <el-card class="img-item" v-for="item in list" :key="item.id">
-               <img @click="selectImg(item)" :src="item.url" alt="">
-           </el-card>
-       </div>
-       <el-row type="flex" justify="center">
-         <el-pagination @current-change="changePage" :current-page="page.page" :page-size="page.pageSize" :total="page.total" background layout="prev, pager, next"></el-pagination>
-       </el-row>
-
+      <div class="img-list">
+        <el-card class="img-item" v-for="item in list" :key="item.id">
+          <img @click="selectImg(item)" :src="item.url" alt />
+        </el-card>
+      </div>
+      <el-row type="flex" justify="center">
+        <el-pagination
+          @current-change="changePage"
+          :current-page="page.page"
+          :page-size="page.pageSize"
+          :total="page.total"
+          background
+          layout="prev, pager, next"
+        ></el-pagination>
+      </el-row>
     </el-tab-pane>
+    <!-- 上传图片 -->
     <el-tab-pane label="上传图片" name="upload">
-
+      <el-upload class="avatar-uploader" action :show-file-list="false" :http-request="uploadImg">
+        <i class="el-icon-plus avatar-uploader-icon"></i>
+      </el-upload>
     </el-tab-pane>
   </el-tabs>
 </template>
@@ -31,6 +41,19 @@ export default {
     }
   },
   methods: {
+    // 上传图片
+    uploadImg (params) {
+      let formData = new FormData()
+      formData.append('image', params.file)
+      this.$axios({
+        url: '/user/images',
+        method: 'post',
+        data: formData
+      }).then(result => {
+        // 获取上传成功之后的地址
+        this.$emit('onSelectImg', result.data.url)
+      })
+    },
     // 选择一张图片
     selectImg (item) {
       // 获取点击图片的地址 item.url
@@ -61,7 +84,6 @@ export default {
     this.getMaterial()
   }
 }
-
 </script>
 
 <style lang="less" scoped>
@@ -79,5 +101,27 @@ export default {
     }
   }
 }
-
+.avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
 </style>
